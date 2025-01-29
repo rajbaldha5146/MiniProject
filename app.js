c0a0d96af7e0c846a27a5a2984c96e221723f326
rajbaldha5146 <161558165+rajbaldha5146@users.noreply.github.com>
2025-01-29 12:16:43 +0530

    Delete NodeModule

--- app.js
+++ app.js
@@ -84,7 +84,7 @@ app.post('/register', async(req, res) => {
                 })
                 let token = jwt.sign({email: email , userid : user._id},"shhh");
                 res.cookie("token",token);
-                res.send("registered");
+                res.redirect("/profile");
             })
         })
     }
